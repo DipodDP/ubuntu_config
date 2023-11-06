196295e6fdaa4d282233f602302b93fbee001159
dipod <dipoddp@gmail.com>
2023-11-06 08:27:24 -0800

    fix(config): changed installation of node.js and netcat

--- my_config.sh
+++ my_config.sh
@@ -19,7 +19,7 @@ sudo apt upgrade -y
 
 # install utils
 sudo apt install ripgrep -y
-sudo apt install netcat -y
+sudo apt install netcat-traditional -y
 sudo apt install bat -y
 sudo apt install lsof -y
 sudo apt install htop -y
@@ -145,8 +145,14 @@ mkdir ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/poetry
 poetry completions zsh > ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/poetry/_poetry
 
 #Install Node.js
-curl -fsSL https://deb.nodesource.com/setup_19.x | sudo -E bash - &&\
-sudo apt install -y nodejs
+sudo apt-get update
+sudo apt-get install -y ca-certificates curl gnupg
+sudo mkdir -p /etc/apt/keyrings
+curl -fsSL https://deb.nodesource.com/gpgkey/nodesource-repo.gpg.key | sudo gpg --dearmor -o /etc/apt/keyrings/nodesource.gpgkey
+NODE_MAJOR=20
+echo "deb [signed-by=/etc/apt/keyrings/nodesource.gpg] https://deb.nodesource.com/node_$NODE_MAJOR.x nodistro main" | sudo tee /etc/apt/sources.list.d/nodesource.list
+sudo apt-get update
+sudo apt-get install nodejs -y
 
 # Install LSP
 # sudo npm i -g pyright
